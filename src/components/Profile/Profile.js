6591c9514c121b007fce7852aa2fdf16c7e6b1ea
paprika-and-fish <david_krajczar1204@yahoo.com>
2021-12-05 00:19:43 +0200

    va ranesc cu tot cu cssu vostru

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,11 +5,15 @@ export default class Profile extends Component {
 
     render() {
         return(
-            <div id="profilebody">
-                <div id="grid-profile">
-                <div id="edit-profile" > Edit profile </div>
-            </div>
-            </div>
-        )
+            <>
+                <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"></link>
+                <div id="profilebody">
+                    <div id="grid-profile">
+                        <div id="edit-profile" > Edit profile </div>
+                    </div>
+                </div>
+            </>
+        );
     }
 }
--- src/components/SignUp/signup.js
+++ src/components/SignUp/signup.js
@@ -30,21 +30,24 @@ export default class SignUp extends Component {
     }
     render() {
         return (
-            <div className="signupbody">
-                <div id="login-grid-signup"> 
-                <div id="user-signup"> Username </div>
-                <input type="text" placeholder="Type your username" id="type-user-signup" autoComplete="off" />
-                <div id="sitename-signup"> Smart<a id="W">R</a>ecycle </div>
-                <div id="email-signup"> Email </div>
-                <input type="email" placeholder="Type your email adress" id="type-email-signup" autoComplete="off" />
-                <div id="pass-signup"> Password </div>
-                <input type="password" placeholder="Type your password" id="type-pass-signup" autoComplete="off"  />
-                <div id="acc-signup"> Have an account? </div>
-                <div id="log-signup"> Sign in </div>
-                <div id="next-signup" onClick = {() => this.createAccount()}> Next </div>
-                <span class="material-icons-outlined" id="visible-signup" onClick={() => this.makeVisible()}> visibility_off </span>
+            <>
+            
+                <div className="signupbody">
+                    <div id="login-grid-signup"> 
+                    <div id="user-signup"> Username </div>
+                    <input type="text" placeholder="Type your username" id="type-user-signup" autoComplete="off" />
+                    <div id="sitename-signup"> Smart<a id="W">R</a>ecycle </div>
+                    <div id="email-signup"> Email </div>
+                    <input type="email" placeholder="Type your email adress" id="type-email-signup" autoComplete="off" />
+                    <div id="pass-signup"> Password </div>
+                    <input type="password" placeholder="Type your password" id="type-pass-signup" autoComplete="off"  />
+                    <div id="acc-signup"> Have an account? </div>
+                    <div id="log-signup"> Sign in </div>
+                    <div id="next-signup" onClick = {() => this.createAccount()}> Next </div>
+                    <span class="material-icons-outlined" id="visible-signup" onClick={() => this.makeVisible()}> visibility_off </span>
+                    </div>
                 </div>
-            </div>
+            </>
         );
     }
 }

07c7e1789fe238de5ea86a19abc14e1cd4f8394f
paprika-and-fish <david_krajczar1204@yahoo.com>
2021-12-04 21:47:08 +0200

    horea branchu 2: electric boogaloo
    B) XD ;P

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styles from '../../css/profile.css'
+
+export default class Profile extends Component {
+
+    render() {
+        return(
+            <div id="grid-profile">
+                <div id="grid-profile">
+                <div id="edit-profile" > Edit profile </div>
+            </div>
+            </div>
+        )
+    }
+}

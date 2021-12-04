135549f5be8d80d666d75149f89cac7642b862da
paprika-and-fish <david_krajczar1204@yahoo.com>
2021-12-04 16:35:56 +0200

    dadadaotelueviatamea

--- src/components/SignUp/signup.js
+++ src/components/SignUp/signup.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+<<<<<<< Updated upstream
 import styles from '../../css/signup.module.css'
+=======
+import styles from '../../css/signup.css'
+>>>>>>> Stashed changes
 
 export default class SignUp extends Component {
     constructor(props) {
         super(props);
     }
-
+    
+    makeVisible() {
+        var x = document.getElementById("type-pass-signup");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
     render() {
         return (
+<<<<<<< Updated upstream
             <div className={styles}>
                 <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
@@ -35,6 +48,22 @@ export default class SignUp extends Component {
                     <div className={styles["next"]}> Next </div>
 
                     <span className={styles["material-icons-outlined-visible"]} onclick="makeVisible()"> visibility_off </span>
+=======
+            <div>
+                <div id="login-grid-signup"> 
+                <div id="user"> Username </div>
+                <input type="text" placeholder="Type your username" id="type-user-signup" autoComplete="off" />
+                <div id="sitename-signup"> Smart<a id="W">R</a>ecycle </div>
+                <div id="email-signup"> Email </div>
+                <input type="email" placeholder="Type your email adress" id="type-email-signup" autoComplete="off" />
+                <div id="pass-signup"> Password </div>
+                <input type="password" placeholder="Type your password" id="type-pass-signup" autoComplete="off"  />
+                <div id="acc-signup"> Have an account? </div>
+                <div id="log-signup"> Sign in </div>
+                <div id="next-signup"> Next </div>
+
+                <span class="material-icons-outlined" id="visible-signup" onClick={() => this.makeVisible()}> visibility_off </span>
+>>>>>>> Stashed changes
                 </div>
             </div>
         );
